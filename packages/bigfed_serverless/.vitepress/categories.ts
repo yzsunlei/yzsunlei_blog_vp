94b86b7371c56f4c10310f4a762411955c01193c
sunlei <sunlei@jxgtmbjt.com>
2024-05-05 13:00:55 +0800

    feat: bigfed_serverless项目初始化

---
+++ packages/bigfed_serverless/.vitepress/categories.ts
@@ -0,0 +1,30 @@
+export default [
+  {
+    text: "简要介绍",
+    items: [
+      { text: 'A.《响应式Web设计:HTML5和CSS3实战》', link: '/01.basis/A.《响应式Web设计:HTML5和CSS3实战》.html', target:'_blank' },
+      { text: 'E.《JavaScript_ES6函数式编程入门经典》', link: '/01.basis/E.《JavaScript_ES6函数式编程入门经典》.html', target:'_blank'}
+    ],
+  },
+  {
+    text: "原理与应用",
+    items: [
+      { text: 'E.《JavaScript设计模式与开发实践》', link: '/02.advance/E.《JavaScript设计模式与开发实践》.html', target:'_blank' },
+      { text: 'K.《Koa与Node.js开发实战》', link: '/02.advance/K.《Koa与Node.js开发实战》.html', target:'_blank' },
+    ],
+  },
+  {
+    text: "平台与项目",
+    items: [
+      { text: 'L.《深入浅出Serverless-技术原理与应用实践》', link: '/03.senior/L.《深入浅出Serverless-技术原理与应用实践》.html', target:'_blank' },
+      { text: 'L.《Web前端黑客技术揭秘》', link: '/03.senior/L.《Web前端黑客技术揭秘》.html', target:'_blank'},
+    ]
+  },
+  {
+    text: "实战场景",
+    items: [
+      { text: '01.数据结构', link: '/04.classify/01.数据结构.html', target:'_blank'},
+      { text: '07.浏览器渲染', link: '/04.classify/07.浏览器渲染.html', target:'_blank'},
+    ]
+  }
+];
---
+++ packages/bigfed_serverless/docs/index.md
@@ -0,0 +1,43 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+title: 小磊哥er的大前端Serverless专题-小磊哥er的博客
+titleTemplate: false
+
+hero:
+  name: 大前端Serverless专题
+  # text: Serverless专题
+  tagline: 学习使用Serverless，极大提升项目开发效率
+  image:
+    src: index.png
+    alt: 大前端Serverless专题
+  actions:
+    - theme: brand
+      text: 快去看看
+      link: /#
+    - theme: alt
+      text: 返回主站
+      link: https://www.yzsunlei.com
+
+features:
+  - title: 简要介绍
+    details: 定义、特点、价值、发展、优劣
+    link: /01.basis/A.《响应式Web设计:HTML5和CSS3实战》.html
+    linkText: 点击去看看
+    target: blank
+  - title: 原理与应用
+    details: 基本原理、相关技术、应用场景
+    link: /01.basis/A.《响应式Web设计:HTML5和CSS3实战》.html
+    linkText: 点击去看看
+    target: blank
+  - title: 平台与项目
+    details: Aws Lambda、阿里云函数计算、OpenWhisk
+    link: /01.basis/A.《响应式Web设计:HTML5和CSS3实战》.html
+    linkText: 点击去看看
+    target: blank
+  - title: 实战场景
+    details: Web Cms、监控告警、图像处理、大数据计算
+    link: /01.basis/A.《响应式Web设计:HTML5和CSS3实战》.html
+    linkText: 点击去看看
+    target: blank
+---
