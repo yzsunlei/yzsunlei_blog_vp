02481823cc53e19498923ec855ead22d8a9b12f5
sunlei <sunlei@jxgtmbjt.com>
2024-05-06 21:39:54 +0800

    feat: bigdata_automation目录整理

---
+++ packages/bigdata_automation/.vitepress/categories.ts
@@ -0,0 +1,40 @@
+export default [
+  {
+    text: "一、文件目录操作",
+    items: [
+      { text: '1.1.读写文件自动化', link: '/01.file_directory/1.1.读写文件自动化.html', target:'_blank' },
+      { text: '1.2.整理文件自动化', link: '/01.file_directory/1.2.整理文件自动化.html', target:'_blank' },
+    ],
+  },
+  {
+    text: "二、Office办公处理",
+    items: [
+      { text: '2.1.Excel表格自动化', link: '/02.office_file/2.1.Excel表格自动化.html', target:'_blank' },
+      { text: '2.2.Word文档自动化', link: '/02.office_file/2.2.Word文档自动化.html', target:'_blank' },
+      { text: '2.3.PPT演示自动化', link: '/02.office_file/2.3.PPT演示自动化.html', target:'_blank' },
+      { text: '2.4.PDF文档自动化', link: '/02.office_file/2.4.PDF文档自动化.html', target:'_blank' },
+    ],
+  },
+  {
+    text: "三、多媒体处理",
+    items: [
+      { text: '3.1.图像处理自动化', link: '/03.media_file/3.1.图像处理自动化.html', target:'_blank' },
+      { text: '3.2.视频剪辑自动化', link: '/03.media_file/3.2.视频剪辑自动化.html', target:'_blank' },
+      { text: '3.3.音频处理自动化', link: '/03.media_file/3.3.音频处理自动化.html', target:'_blank' },
+    ]
+  },
+  {
+    text: "四、营销通知",
+    items: [
+      { text: '4.1.邮件自动化', link: '/04.marketing_notification/4.1.邮件自动化.html', target:'_blank'},
+      { text: '4.2.短信自动化', link: '/04.marketing_notification/4.2.短信自动化.html', target:'_blank'},
+    ]
+  },
+  {
+    text: "五、软件操作",
+    items: [
+      { text: '5.1.网站浏览器自动化', link: '/05.software_application/5.1.网站浏览器自动化.html', target:'_blank'},
+      { text: '5.2.桌面软件自动化', link: '/05.software_application/5.2.桌面软件自动化.html', target:'_blank'},
+    ]
+  }
+];
---
+++ packages/bigdata_automation/docs/index.md
@@ -0,0 +1,59 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+title: 小磊哥er的大数智办公自动化专题-小磊哥er的博客
+titleTemplate: false
+
+hero:
+  name: 大数智办公自动化专题
+  # text: 大数智办公自动化专题
+  tagline: 学习使用办公自动化技术，极大提升日常办公效率
+  image:
+    src: index.png
+    alt: 大数智办公自动化专题
+  actions:
+    - theme: brand
+      text: 快去看看
+      link: /preface.html
+    - theme: alt
+      text: 返回主站
+      link: https://www.yzsunlei.com
+
+features:
+  - title: 文件目录操作
+    details: 读写文件、整理文件
+    link: /01.file_directory/index.html
+    linkText: 点击去看看
+    target: blank
+  - title: Office办公处理
+    details: Excel处理、Word处理、PPT生成、PDF生成
+    link: /02.office_file/index.html
+    linkText: 点击去看看
+    target: blank
+  - title: 多媒体处理
+    details: 图像处理、视频剪辑、音频处理
+    link: /03.media_file/index.html
+    linkText: 点击去看看
+    target: blank
+  - title: 营销通知
+    details: 邮件营销、短信通知
+    link: /04.marketing_notification/index.html
+    linkText: 点击去看看
+    target: blank
+  - title: 软件操作
+    details: 浏览器操作、桌面软件操作
+    link: /05.software_application/index.html
+    linkText: 点击去看看
+    target: blank
+---
+
+<style>
+  @media (min-width: 960px) {
+    .image-container {
+      transform: none !important;
+    }
+    .item.grid-4 {
+      width: calc(100% / 3) !important;
+    }
+  }
+</style>
